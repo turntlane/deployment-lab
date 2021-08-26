ac37bfcbef99c8591aa65451247868de3e5f736d
Jake Alexander <imjakeal123@gmail.com>
2021-08-26 14:38:05 -0600

    added css endpoint

--- server/index.js
+++ server/index.js
@@ -7,6 +7,10 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../index.html'));
 })
 
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../style.css'));
+})
+
 const port = process.env.PORT || 4005;
 
 app.listen(port, () => {
